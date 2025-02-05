727316e64b4d0cf37af98b95ca5863a1921a0f80
wickywaa <gavnewton27@gmail.com>
2025-02-06 00:56:15 +0100

    featre

---
+++ admin-frontend/src/components/CreateGames/CockpitAutoComplete.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IConnectedCockpit } from '../../models'
+
+
+interface ICockpitAutoComplete {
+  cockpit: IConnectedCockpit
+}
+
+export const CockpitAutoComplete:React.FC<ICockpitAutoComplete> = ({cockpit}) => {
+  return (
+    <div></div>
+  )
+
+}
